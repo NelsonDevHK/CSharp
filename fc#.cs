cbbd5d4f463d798925825ac969f44a427bc6a5b4
NelsonDevHK <songkuenhk@gmail.com>
2020-07-18 17:37:34 +0800

    BubbleSort

--- fc#.cs
+++ fc#.cs
@@ -2,12 +2,26 @@ using System;
 
 namespace Project_1 
 {
-    class MainClass {
+    class MainClass
+     {
         public static void Main (string[] args) 
         {
-
-            Console.WriteLine ("test connect to git2!");
-            Console.ReadKey ();
+            int[] arr = { 3, 3, 2, 1, 3, 2, 1};
+         int temp;
+         for (int j = 0; j <= arr.Length - 2; j++) {
+            for (int i = 0; i <= arr.Length - 2; i++) {
+               if (arr[i] > arr[i + 1]) {
+                  temp= arr[i + 1];
+                  arr[i + 1] = arr[i];
+                  arr[i] = temp;
+               }
+            }
+         }
+         Console.WriteLine("Sorted:");
+         foreach (int p in arr)
+            Console.Write(p + " ");
+         Console.Read();
+ 
         }
     }
 }
